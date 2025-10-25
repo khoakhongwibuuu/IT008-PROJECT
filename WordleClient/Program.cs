a99b8cde1beba57eb9c09c30053b0599ef15c592
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-10-25 20:38:36 +0700

    OK

--- WordleClient/Program.cs
+++ WordleClient/Program.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Linq;
-using System.Diagnostics;
-using WordleClient.libraries.lowlevel;
-using WordleClient.libraries.ingame;
-using WordleClient.tests;
-
 namespace WordleClient
 {
     internal static class Program
@@ -12,14 +5,13 @@ namespace WordleClient
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        //[STAThread]
+        [STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1());
-            //TestWDBReader.Exec();
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainMenu());
         }
     }
 }
