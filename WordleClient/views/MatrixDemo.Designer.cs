239e94b44631d067c5fc907c332b4139640b9a57
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-10-26 22:24:41 +0700

    Add components to test Matrix of Charbox

---
+++ WordleClient/views/MatrixDemo.Designer.cs
@@ -0,0 +1,34 @@
+﻿namespace WordleClient
+{
+    partial class MatrixDemo
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MatrixDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Name = "MatrixDemo";
+            this.Text = "Matrix Demo";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
