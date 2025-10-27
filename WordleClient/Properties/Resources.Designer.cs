d229cde1504fb8cb1861a062f6e0ccefc0d2bdb2
Khiêm Trần Thái <24520806@ms.uit.edu.vn>
2025-10-27 16:39:33 +0700

    Update

--- WordleClient/Properties/Resources.Designer.cs
+++ WordleClient/Properties/Resources.Designer.cs
@@ -59,25 +59,5 @@ namespace WordleClient.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap close {
-            get {
-                object obj = ResourceManager.GetObject("close", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap settings {
-            get {
-                object obj = ResourceManager.GetObject("settings", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
