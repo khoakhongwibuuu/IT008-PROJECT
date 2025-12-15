d44e8fbb9246bb3bfd3a5f2ab6f1884b48f5cc1d
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-12-15 11:39:22 +0700

    Centralised Network modules

---
+++ WordleClient/libraries/network/ServerManager.cs
@@ -0,0 +1,33 @@
+﻿using WordleClient.libraries.network;
+
+namespace WordleClient.libraries.network
+{
+    public static class ServerManager
+    {
+        private static readonly ServerListener server = new();
+
+        public static ServerListener Server => server;
+
+        public static bool IsRunning { get; private set; }
+
+        public static Task? ServerTask { get; private set; }
+
+        public static void Start(int port)
+        {
+            if (IsRunning)
+                return;
+
+            IsRunning = true;
+            ServerTask = server.StartAsync(port);
+        }
+
+        public static void Stop()
+        {
+            if (!IsRunning)
+                return;
+
+            server.Stop();
+            IsRunning = false;
+        }
+    }
+}
