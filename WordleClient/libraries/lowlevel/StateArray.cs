300d4bc1ec6015b68115e4654c53d4545220748d
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-12-24 19:26:42 +0700

    New network transmission friendly StateArray

--- WordleClient/libraries/lowlevel/StateArray.cs
+++ WordleClient/libraries/lowlevel/StateArray.cs
@@ -7,58 +7,52 @@ namespace WordleClient.libraries.lowlevel
         INVALID_ORDER = 1,
         MATCH = 2
     }
-    public class StateArray : IEnumerable<TriState>, IEnumerable
+    [Serializable]
+    public sealed class StateArray : IEnumerable<TriState>
     {
-        private readonly byte[] data;
-        public int Length
+        private byte[] _data;
+        public StateArray()
         {
-            get;
+            _data = Array.Empty<byte>();
         }
         public StateArray(int length)
         {
-            ArgumentOutOfRangeException.ThrowIfNegative(length);
-            Length = length;
-            data = new byte[(length + 0b11) >> 2];
+            _data = new byte[length];
+        }
+        public int Length => _data.Length;
+        public TriState Get(int index)
+        {
+            return (TriState)_data[index];
         }
         public void Set(int index, TriState value)
         {
-            if ((uint)index >= (uint)Length) throw new IndexOutOfRangeException();
-            int byteIndex = index >> 2;
-            int bitOffset = (index & 0b11) << 1;
-            byte mask = (byte)(0b11 << bitOffset);
-            data[byteIndex] = (byte)(data[byteIndex] & ~mask | ((byte)value & 0b11) << bitOffset);
+            _data[index] = (byte)value;
         }
-        public TriState Get(int index)
+        public TriState this[int index]
         {
-            if ((uint)index >= (uint)Length) throw new IndexOutOfRangeException();
-            int byteIndex = index >> 2;
-            int bitOffset = (index & 0b11) << 1;
-            return (TriState)(data[byteIndex] >> bitOffset & 0b11);
-        }
-        public void SetAll(TriState value)
-        {
-            byte pattern = 0;
-            byte valBits = (byte)((byte)value & 0b11);
-            for (int i = 0; i < 4; i++) pattern |= (byte)(valBits << i * 2);
-            Array.Fill(data, pattern);
+            get => (TriState)_data[index];
+            set => _data[index] = (byte)value;
         }
         public bool IsFullValue(TriState value)
         {
-            for (int i = 0; i < Length; i++)
+            byte b = (byte)value;
+            for (int i = 0; i < _data.Length; i++)
             {
-                if (Get(i) != value) return false;
+                if (_data[i] != b)
+                    return false;
             }
             return true;
         }
         public IEnumerator<TriState> GetEnumerator()
         {
-            for (int i = 0; i < Length; i++) yield
-            return Get(i);
+            for (int i = 0; i < _data.Length; i++)
+                yield return (TriState)_data[i];
         }
         IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+        public byte[] ToByteArray()
         {
-            return GetEnumerator();
+            return (byte[])_data.Clone();
         }
-        public byte[] RawData => data;
     }
 }
